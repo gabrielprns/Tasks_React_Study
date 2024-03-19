8bc517a791539d65df9e994d6c86dc9f3aabba06
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2024-03-19 14:17:27 -0300

    Redux

--- redux-guide-starter/src/redux/cart/reducer.js
+++ redux-guide-starter/src/redux/cart/reducer.js
@@ -8,14 +8,34 @@ const initialState = {
 const cartReducer = (state = initialState, action)=>{
   switch (action.type){
     case CartActionTypes.ADD_PRODUCT:
-      initialState.products.push(action.payload)
-      return {
-        ...initialState,
-        products: [...initialState.products, action.payload],
+      //verificar se o produto já está no carrinho
+      const productIsAlreadyInCart = state.products.some(
+        (product)=> product.id == action.payload.id
+      );
+
+      //se estiver, aumentar a sua quantidade em 1
+
+      if(productIsAlreadyInCart){
+        return{
+          ...state,
+          products:state.products.map((product)=>
+            product.id == action.payload.id
+            ?{ ...product, quantity: product.quantity + 1}
+            :product
+          )
+        }
+      }
+
+      //se ele não estiver adiciona-lo
+      return{
+        ...state,
+        products: [...state.products, {...action.payload, quantity: 1}]
+      }
+
+       
       
-  }
     default:
       return state;
-}}
+  }}
 
 export default cartReducer
