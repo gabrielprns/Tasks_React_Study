d1de7d80ee98a65148ab0b696e233fa8e97fa1b3
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2024-03-24 00:00:25 -0300

    Remove Item

--- redux-guide-starter/src/redux/cart/actions.js
+++ redux-guide-starter/src/redux/cart/actions.js
@@ -4,3 +4,9 @@ export const addProductToCart = (payload) => ({
   type: CartActionTypes.ADD_PRODUCT,
   payload
 })
+
+
+export const removeProductFromCart = (payload) => ({
+  type: CartActionTypes.REMOVE_PRODUCT,
+  payload
+})
